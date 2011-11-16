e3a6292b911e62a24ba84d38382a1c8cbe32aaad
kuanggong <kuanggong@a61af939-5630-496a-2fff-aa91c4831e75>
2011-11-16 22:38:19 +0000

    starting restructuring.

---
+++ EditorTab.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+import os
+import io
+from tkinter import *
+from tkinter import ttk
+
+from options import *
+
+class EditorTab(ttk.Frame):
+    def __init__(self, parent=None, file=None):
+        ttk.Frame.__init__(self, parent)
+        self.pack(expand=YES, fill=BOTH)
+    
+
+if __name__ == '__main__':
+    ''' test the EditorTab class '''
+    root = Tk()
+    root.title(string='程序本 v0.01')
+    
+    frame=ttk.Frame(root)
+    tab=EditorTab(frame)
+    
+    frame.pack(expand=YES,fill=BOTH)
+    root.mainloop()
+            
