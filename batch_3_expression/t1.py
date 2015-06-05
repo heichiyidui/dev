9ada5b4d58fab69cf3a7b0afe4c758671b092215
Kuang Lin <kuanggong@gmail.com>
2015-06-05 15:39:52 +0100

    adding new pheno tables

--- batch_3_expression/t1.py
+++ batch_3_expression/t1.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
 
-ifile=open('t.in')
-for line in ifile:
-    cols=line.split('\t')
-    print(cols[18:-4].count('FALSE'),cols[18:-4].count('TRUE'))
-        
+for n in range(2, 10):
     
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
