e144029ff9c177ed4c9de720c4b0e7320b70c01c
Kuang Lin <kuanggong@gmail.com>
2015-06-11 16:40:24 +0100

    adding new linear regression project

---
+++ trem2/trem2.zsh
@@ -0,0 +1,19 @@
+################################################################################
+# TREM2 analysis                                                               #
+################################################################################
+
+
+
+
+
+################################################################################
+#                                                                              #
+# First filter the probesets e.g. subset of probes that can be reliably        #
+# detected in 80% of samples in at least one diagnostic group                  #
+#                                                                              #
+# Then, the effects of age, gender, collection site and RIN should be          #
+# regressed out of the normalised gene expression data and the corresponding   #
+# residuals compared between diagnostic groups (control, MCI and AD) using     #
+# linear models followed by post-hoc t-tests.                                  #
+#                                                                              #
+################################################################################
