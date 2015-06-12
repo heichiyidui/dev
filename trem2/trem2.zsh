0fee47d2d27149202ecf0a4b4194025fb576cdfe
Kuang Lin <kuanggong@gmail.com>
2015-06-12 16:49:19 +0100

    minor changes in trem2 linear

--- trem2/trem2.zsh
+++ trem2/trem2.zsh
@@ -2,10 +2,6 @@
 # TREM2 analysis                                                               #
 ################################################################################
 
-
-
-
-
 ################################################################################
 #                                                                              #
 # First filter the probesets e.g. subset of probes that can be reliably        #
@@ -17,3 +13,25 @@
 # linear models followed by post-hoc t-tests.                                  #
 #                                                                              #
 ################################################################################
+
+########################################
+# 1. subjects
+
+# according to the 'Diagnosis at sampling TREM2+' column in 
+# TREM2_cytokine_analysis_demographics.xlsx
+# 64 AD
+# 22 Control
+# 17 MCI
+#  1 FTD
+#  3 mixed/DLB
+#  3 PD
+#  1 unknown
+
+########################################
+# 2. probes 
+
+# 30 probes, 111 subjects, 3330 values
+# 1392 undetected...
+
+# need more than 51 AD detections or more than 17 CTL detections
+
