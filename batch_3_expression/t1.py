51785e5ff141c2119e00dbf30ec1a336656dc990
Kuang Lin <kuanggong@gmail.com>
2015-07-14 16:59:09 +0100

    adding permutation results

--- batch_3_expression/t1.py
+++ batch_3_expression/t1.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3 
 
+import numpy
+
 ifile=open('t.in')
+
+predicts=[]
+eu_codes=[]
 for line in ifile:
     cols=line[:-1].split()
-    if len(cols) == 2:
-        print(cols[0],cols[1])
-    else: 
-        print(cols[0],cols[2])
-        print(cols[1],cols[2])
-
+    predicts.append(cols[1])
+    eu_codes.append(cols[3])
+    
 ifile.close()
+
+import random
+random.seed(514)
+for l in range(100000):
+    sum_identical=0
+    random.shuffle(predicts)
+    
+    for i in range(len(eu_codes)):
+        for j in range(i+1,len(eu_codes)):
+            if eu_codes[i] == eu_codes[j]:
+                if predicts[i] == predicts[j]:
+                    sum_identical += 1
+    print(sum_identical)
