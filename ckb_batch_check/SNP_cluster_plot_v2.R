c9b0645c21c72d8d42fb66b64569fb76f5cd62d6
heichiyidui <kuanggong@gmail.com>
2016-04-13 21:37:09 +0100

    minor color change

--- ckb_batch_check/SNP_cluster_plot_v2.R
+++ ckb_batch_check/SNP_cluster_plot_v2.R
@@ -69,8 +69,8 @@ for (snp_id in snp_ids)
 
     xtemp <- max(abs(c(x3 + 2*sqrt(vx3), x1 - 2*sqrt(vx1), snp_avm$M) ))
 
-    snp_cols   = c('0' = 'red', '1' = '#999900', '2' = 'blue', '3' = '#606060')
-    snp_shapes = c('0' = 24,    '1' = 21,        '2' = 25,     '3' = 22)
+    snp_cols   = c('0' = 'red', '1' ='#999900', '2' ='#99CCFF', '3' ='#9933FF')
+    snp_shapes = c('0' = 24,    '1' = 21,       '2' = 25,       '3' = 22)
                     # 'AA', 'AB', 'BB' and 'missing'
                     # must be specific, otherwise if one of 'AA' etc is not
                     # presented, 'missing' will be in a different colour.
@@ -78,7 +78,7 @@ for (snp_id in snp_ids)
     p <- ggplot() +
         geom_point(data = snp_avm,
                    aes(x=M, y=A, shape=called, color=called, fill=called),
-                   alpha=0.9, size=3) +
+                   alpha=0.5, size=3) +
         scale_colour_manual(values=snp_cols) +
         scale_fill_manual  (values=snp_cols) +
         scale_shape_manual (values=snp_shapes) +
