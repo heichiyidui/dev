9da92cd76ee80b845cbca03f7f191b0ba2d7fd6d
heichiyidui <kuanggong@gmail.com>
2016-04-18 16:26:03 +0100

    got another 3000 SNPs done

--- ckb_batch_check/ckb_batch_check.zsh
+++ ckb_batch_check/ckb_batch_check.zsh
@@ -270,13 +270,14 @@ cat plate_chk_res.ls batch_v2_chk_res.ls | awk '{print $1}' | \
 
 rm  to_exam_png/*.png
 
-awk '{print $1}' to_exam/xah | sort > t1.ls
+awk '{print $1}' to_exam/xak | sort > t1.ls
 grab -f examed.ls -v t1.ls > to_exam.ls
 
 awk '{print "cp to_exam_png.bak/" $1 "_comb.png to_exam_png/"}' to_exam.ls \
     > t.out
 source t.out
 
+geeqie to_exam_png/
 # now check the png pictures in the to_exam_png directory
 # delete pictures of badly called snps
 
