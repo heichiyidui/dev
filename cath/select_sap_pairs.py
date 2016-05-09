9f91135863a7eb6bc90eceb5ab885bda75014bf1
heichiyidui <kuanggong@gmail.com>
2016-05-09 11:31:33 +0100

    minor changes

--- cath/select_sap_pairs.py
+++ cath/select_sap_pairs.py
@@ -11,30 +11,39 @@ for line in ifile:
     else:
         cath_class[cath].append(dom_id)
 ifile.close()
-print(len(cath_class))
+# print(len(cath_class))
 
 keyset=set(cath_class.keys())
 for cath in keyset:
     if len(cath_class[cath]) < 2:
         del cath_class[cath]
-
-print(len(cath_class))
+# print(len(cath_class))
 
 import random
 random.seed(514)
 
-pair_set=set()
-for i in range(40):
+pair_set = set()
+used_domain_set = set()
+for i in range(10):
     for cath in cath_class.keys():
         random.shuffle(cath_class[cath])
+        if cath_class[cath][0] in used_domain_set:
+            continue
+        if cath_class[cath][1] in used_domain_set:
+            continue
         pair=sorted(cath_class[cath][0:2])
         pair_set.add(' '.join(pair))
+        used_domain_set.add(cath_class[cath][0])
+        used_domain_set.add(cath_class[cath][1])
+    # print(len(pair_set))
 
-# for p in pair_set:
-#     id1,id2=p.split()
-#     print('~/bin/sap','dompdb/'+id1,'dompdb/'+id2,'> sap_aln/'+id1+'_'+id2)
 
-# 1427 pairs in the first iteration
-#  836 in the second
-#  762 in the third ...
+for p in pair_set:
+    id1,id2=p.split()
+    print('~/bin/sap','dompdb/'+id1,'dompdb/'+id2,'> sap_aln/'+id1+'_'+id2)
 
+# 1221 pairs added in the first iteration
+#  342 in the second
+#  285 in the third ...
+# 2957 pairs after 10 iterations
+# The number might change because of different random seeds.
